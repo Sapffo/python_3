df82ebd07668b3437345b04bba30a1a2b549a67f
Вероника Фасхутдинова <sapffo@yandex.ru>
2022-10-05 20:55:43 +0300

    home_work_5

--- 5.py
+++ 5.py
@@ -1,10 +1,15 @@
 #.Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов
-# a=int(input('введите число'))
-# f1=f2=-10
-# for i in range(a):
-#     f1,f2=f2,f1+f2
-#     print(f2)
-# # list=[]
-# # for i in range(f2):
-# #     list.append((f2))
-# # print(list)
+a=int(input('введите число'))
+b=int(input('введите число'))
+
+f3=1
+f1=0
+f2=1
+for i in range(a):
+    f1,f2=f2,f1+f2
+   #f1,f3=f3,f1-f3
+    print(f2)
+    #print(f3)
+for j in range (b):
+    f1,f3=f3,f1-f3
+    print(f3)
